f1e55b1e7776a284cb8584693129b036073993d0
Your Name <you@example.com>
2026-09-03 11:33:33 +0200

    adding yml

--- src/test/java/dk/itu/datasys/EngineTest.java
+++ src/test/java/dk/itu/datasys/EngineTest.java
@@ -1,5 +1,11 @@
 package dk.itu.datasys;
 
-public class EngineTest {
-    
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class EngineTest {
+    @Test
+    void teamName() {
+        assertEquals("Team <your team name>", new Engine().teamName());
+    }
 }
